e44f9448463158bacd0ce9cf6dfa3c0b358a3d58
Riya Bhangale <riyabhangale14@gmail.com>
2019-06-15 00:21:01 -0500

    New Test

--- app.py
+++ app.py
@@ -1,11 +1,30 @@
-from flask import Flask
+from flask import Flask, render_template, request
+import sqlite3 as sql
+import pandas as pd
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def home():
+    return render_template('home.html')
+
+
+@app.route('/upload')
+def upload_csv():
+    return render_template('upload.html')
+
+
+@app.route('/adddata',methods = ['POST', 'GET'])
+def adddata():
+   if request.method == 'POST':
+       con = sql.connect("database.db")
+       csv = request.files['myfile']
+       file = pd.read_csv(csv)
+
+       file.to_sql('Earthquake', con, schema=None, if_exists='replace', index=True, index_label=None, chunksize=None, dtype=None)
+       con.close()
+       return render_template("adddata.html", msg = "Record inserted successfully")
 
 
 if __name__ == '__main__':
