b6d48b8ae237ea921aaab1e772736bb604ce4e48
Riya Bhangale <riyabhangale14@gmail.com>
2019-06-17 16:26:18 -0500

    Redis display All

--- app.py
+++ app.py
@@ -2,8 +2,17 @@ from flask import Flask, render_template, request,flash
 import sqlite3 as sql
 import pandas as pd
 from time import time
+import redis
+import pickle
 
 app = Flask(__name__)
+
+r = redis.StrictRedis(host='riyacloud.redis.cache.windows.net', port=6380, db=0, password='593PO2XLFETRrSYj2sSnOzznMqlLgrKjKiaCMQEB7Fw=', ssl=True)
+# result = r.ping()
+# print("Ping returned : " + str(result))
+# r.set("msg:hello","Hello Redis!!!")
+# msg = r.get("msg:hello")
+# print(msg)
 
 
 @app.route('/')
@@ -21,7 +30,7 @@ def addAlldata():
    if request.method == 'POST':
        start_time = time()
        conn = sql.connect("database.db")
-       csvFile = request.files['myfile']
+       csvFile = request.files['myfileCSV']
        data = pd.read_csv(csvFile)
 
        data.to_sql('Earthquake', conn, schema=None, if_exists='replace', index=True, index_label=None, chunksize=None, dtype=None)
@@ -33,13 +42,54 @@ def addAlldata():
 
 
 @app.route('/displayAll')
-def displayAll():
+# def displayAll():
+    # conn = sql.connect("database.db")
+    # c = conn.cursor()
+    # query = "SELECT * FROM Earthquake"
+    # c.execute(query)
+    # rows = c.fetchall()
+    # return render_template('display.html',data = rows, len=len(rows))
+#
+def display():
+    keyname = 'displayAll'
+    if(r.exists(keyname)):
+        is_Cache = 'Query with Cache'
+        start_time = time()
+        rows = pickle.loads(r.get(keyname))
+        end_time = time()
+        time_taken = (end_time - start_time)
+        print(time_taken,is_Cache)
+        r.delete(keyname)
+    else:
+        is_Cache = 'Query without Cache'
+        start_time = time()
+        conn = sql.connect("database.db")
+
+        c = conn.cursor()
+        c.execute("select * from Earthquake")
+
+        rows = c.fetchall()
+        end_time = time()
+        time_taken = (end_time - start_time)
+        print(time_taken, is_Cache)
+        print(len(rows))
+        conn.close()
+        r.set(keyname, pickle.dumps(rows))
+    return render_template("display.html", data=rows, time=time_taken, isCache=is_Cache)
+
+
+@app.route('/display1000')
+def display1000():
     conn = sql.connect("database.db")
     c = conn.cursor()
-    query = "SELECT * FROM Earthquake"
-    c.execute(query)
-    rows = c.fetchall()
-    return render_template('display.html',info = rows)
+    start_time = time()
+    for i in range(1000):
+        query = "SELECT * FROM Earthquake"
+        c.execute(query)
+        rows = c.fetchall()
+    end_time = time()
+    time_taken = (end_time - start_time)
+    return render_template('display1000.html',info = rows, time = time_taken)
 
 
 if __name__ == '__main__':
